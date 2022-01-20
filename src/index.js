e81afc37546c2b42908aa60b6a47ef576bedee81
Aliaksandr Fisunou <aliaksandr.fisunou@oracle.com>
2022-01-20 20:16:38 +0300

    project setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './assets/styles.scss'
+
+const teamSectionTemplate = require('./assets/team-section/team-section.handlebars')
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Team Section component start
+    const teamSection = document.createElement('section')
+    teamSection.innerHTML = teamSectionTemplate({
+        title: 'Meet Our Team',
+        description:
+            'Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics',
+        team: [
+            { name: 'Avie Beaton', position: 'CO Founder' },
+            { name: 'Ben Jonson', position: 'Consultant' },
+            { name: 'Ashley Fletcher', position: 'CEO' },
+        ],
+    })
+    teamSection.classList.add('team-section')
+    // Team Section component end
+
+    document.body.appendChild(teamSection)
+})
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        watchFiles: ['src/**/*'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/assets/index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin({
+            cache: false,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.handlebars$/,
+                loader: 'handlebars-loader',
+            },
+        ],
+    },
+}
