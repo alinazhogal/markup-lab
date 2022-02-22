4bb6894907bca037b03facf6b8a3d9a0bf289f81
Alina <alina.zhogal.00@gmail.com>
2022-02-23 01:01:33 +0300

    feat (2header): close input/menu on outer click

--- src/index.js
+++ src/index.js
@@ -15,37 +15,34 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.append(header)
 
     const search = document.querySelectorAll('.nav-input')
-    const searchButton = document.querySelectorAll('.search-button')
 
     document.addEventListener('click', (e) => {
         const { target } = e
-        for (let i = 0; i < searchButton.length; i += 1) {
-            for (let j = 0; j < search.length; j += 1) {
-                const inputOpen = search[j].classList.contains('open')
-                if (!target.classList.contains('search-button') && inputOpen) {
-                    search[j].classList.remove('open')
-                }
-                if (target.closest('.search-button')) {
-                    search[j].classList.toggle('open')
-                }
+        for (let j = 0; j < search.length; j += 1) {
+            if (
+                !(
+                    target.closest('.search-button') ||
+                    target.classList.contains('nav-input')
+                )
+            ) {
+                search[j].classList.remove('input-open')
+            }
+            if (target.closest('.search-button')) {
+                search[j].classList.toggle('input-open')
             }
         }
     })
 
-    // for (let i = 0; i < searchButton.length; i+=1) {
-    // searchButton[i].addEventListener('click', (e) => {
-    //     console.log(e.target)
-    //     for (let j = 0; j < search.length; j+=1) {
-    //         if (e.target !== searchButton[i]) {
-    //             search[j].classList.remove('open')
-    //         }
-    //     search[j].classList.toggle('open')}
-
-    // })}
-
-    const burgerButton = document.querySelector('.burger-button')
-    const mobileMenu = document.querySelector('.mobile-menu')
-    burgerButton.addEventListener('click', () => {
-        mobileMenu.classList.toggle('mobile-menu-open')
+    document.addEventListener('click', (ev) => {
+        const { target } = ev
+        const mobileMenu = document.querySelector('.mobile-menu')
+        if (
+            !(target.closest('.nav-list') || target.closest('.burger-button'))
+        ) {
+            mobileMenu.classList.remove('mobile-menu-open')
+        }
+        if (target.closest('.burger-button')) {
+            mobileMenu.classList.toggle('mobile-menu-open')
+        }
     })
 })
