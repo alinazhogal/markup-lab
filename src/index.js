0d9401ccbfc01f28ed837aa6d21cf031bd48c435
Alina <alina.zhogal.00@gmail.com>
2022-02-22 14:47:11 +0300

    feat (2header): show input

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import './styles.scss'
 
-// const teamSectionTemplate = require('./components/team-section/team-section.handlebars')
 const headerTemplate = require('./components/header/header.handlebars')
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     // Team Section component start
-//     const teamSection = document.createElement('section')
-//     teamSection.innerHTML = teamSectionTemplate({
-//         title: 'Meet Our Team',
-//         description:
-//             'Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics',
-//         team: [
-//             { name: 'Avie Beaton', position: 'CO Founder' },
-//             { name: 'Ben Jonson', position: 'Consultant' },
-//             { name: 'Ashley Fletcher', position: 'CEO' },
-//         ],
-//     })
-//     teamSection.classList.add('team-section')
-//     // Team Section component end
-
-//     document.body.appendChild(teamSection)
-// })
 
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.createElement('header')
@@ -34,4 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     header.classList.add('header')
     document.body.append(header)
+    const search = document.querySelector('.nav-input')
+    const searchButton = document.querySelector('.search-button')
+
+    searchButton.addEventListener('click', () => {
+        search.classList.toggle('display')
+    })
 })
